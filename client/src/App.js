5c51aec2fc9373cf2ca4667be7b9d2f2d445fb10
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-09-17 18:31:33 -0400

    MVP for parts 1 and 2

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path='/' component={MovieList} />
-      <Route path = '/movies/:id' component={Movie} />
+      
+  <Route exact path='/' render={()=> < MovieList /> }/>
+  <Route path='/movies/:id' render={props => <Movie {...props} /> }/>
+      
     </div>
   );
 };

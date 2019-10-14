b83d99eba13c2629e488bee523052d37ae0398d5
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-14 12:28:33 -0400

    initial commit, wrapped app in Router and added routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
 
@@ -12,9 +13,15 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path = '/' component = {MovieList} />
+      <Route path = '/movies/:id' render={props => <Movie {...props} addToSavedList = {this.addToSavedList} /> } />
     </div>
   );
 };
 
 export default App;
+
+
+// Inside your App file add two routes.
+// one route for / that loads the MovieList component.
+// one route that will take an id parameter after/movies/ (ex: /movies/2, /movies/3 where the id is dynamic). This route should load the Movie component.

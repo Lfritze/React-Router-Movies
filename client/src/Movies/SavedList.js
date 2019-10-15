c63eed0b7cb0c4918a47d3d1022515bfd12b05b9
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-10-15 18:07:07 -0400

    added button via styled-componets

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components'
+
+const SavedButton = styled.button`
+background-color: lightseagreen;
+border-radius: 10px;
+border: 2px solid palevioletred;
+color: palevioletred;
+margin: 0 1em;
+padding: 0.25em 1em;
+`;
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-       <NavLink to={`/movies/${movie.id}`} key={movie.id}>
+       <SavedButton><NavLink to={`/movies/${movie.id}`} key={movie.id}>
       <span className="saved-movie">{movie.title}</span>
-      </NavLink>
+      </NavLink></SavedButton>
     ))}
     <NavLink to = '/' >
     <div className="home-button">Home</div></NavLink>
